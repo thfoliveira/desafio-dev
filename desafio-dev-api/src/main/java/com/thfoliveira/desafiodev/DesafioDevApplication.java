aacdf32c0a81907110672fa67e76b5bb872e0ef4
Thiago Oliveira <thiago.oliveira@impa.br>
2021-12-04 03:03:10 -0300

    Base do Projeto Java utilizando Spring Boot, com Configuração de Banco de Dados

---
+++ desafio-dev-api/src/main/java/com/thfoliveira/desafiodev/DesafioDevApplication.java
@@ -0,0 +1,13 @@
+package com.thfoliveira.desafiodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDevApplication.class, args);
+	}
+
+}
